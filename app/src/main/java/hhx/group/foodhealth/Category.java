052888f09f4901947dec621380b1cd971f88ff67
xiaotinghuang <rowenahxt@gmail.com>
2017-10-07 14:33:09 +1100

    Food Health-1007

---
+++ app/src/main/java/hhx/group/foodhealth/Category.java
@@ -0,0 +1,31 @@
+package hhx.group.foodhealth;
+
+/**
+ * Created by Xiaoting Huang on 2017-10-4.
+ */
+
+public class Category {
+
+    private String id;
+    private String name;
+    private String image;
+
+
+    public Category(String id, String name, String image) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
